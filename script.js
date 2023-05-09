16ffa91555c89abcd45563a5cd75acdf24f721e4
SoulRvr29 <130653843+SoulRvr29@users.noreply.github.com>
2023-05-09 11:45:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 let shareButton = document.querySelector("button");
 let author = document.querySelector(".author");
 let socialMenu = document.querySelector(".social");
-let authorBar = document.querySelector(".authorBar");
-let barBkg = document.querySelector(".barBkg");
 
 let button = false;
 
 shareButton.addEventListener("click", function () {
   if (button == false) {
-    socialMenu.style.display = "flex";
-    authorBar.classList.add("barBkg");
+    socialMenu.style.opacity = "100%";
     author.classList.add("visibility");
     button = true;
   } else {
-    socialMenu.style.display = "none";
-    authorBar.classList.remove("barBkg");
+    socialMenu.style.opacity = "0%";
     author.classList.remove("visibility");
     button = false;
   }
